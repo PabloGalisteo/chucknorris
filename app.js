b87dfc3ac5a35947fb3954d2ea7485519d6b37a0
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-03-25 18:31:57 +0100

    trying out chuck norris API

---
+++ app.js
@@ -0,0 +1,29 @@
+document.querySelector('.button-primary').addEventListener('click', getJokes)
+const joke = document.querySelector('.joke')
+
+function getJokes(e) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'http://api.icndb.com/jokes/random', true)
+
+    xhr.onload = function() {
+        if(this.status === 200) {
+            const response = JSON.parse(this.responseText)
+
+            let output = response.value.joke
+
+            if(response.type === 'success') {
+             return joke.innerHTML = output
+                };
+            } else {
+                output +=  '<h2>Chuck Norris is sick</h2>'
+            }
+            
+            
+        }
+        xhr.send()
+    
+        e.preventDefault()
+    }
+
+
