646ee15226e4b93c435d3b1adcf0c185c7a0e51f
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-03-25 18:40:06 +0100

    changed API from http to https

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const joke = document.querySelector('.joke')
 function getJokes(e) {
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'http://api.icndb.com/jokes/random', true)
+    xhr.open('GET', 'https://api.icndb.com/jokes/random', true)
 
     xhr.onload = function() {
         if(this.status === 200) {
